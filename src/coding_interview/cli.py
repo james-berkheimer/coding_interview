403f90559ba3d43cc64f6d8299f65fa8cc86cadb
James Berkheimer <5049386+james-berkheimer@users.noreply.github.com>
2024-04-21 18:17:55 -0700

    James berkheimer/query optimizations (#1)
    * "Added async support to MetQuery class and CLI options for async execution"
    
    Details:
    - Added `use_async` parameter to `MetQuery` class to switch between synchronous and asynchronous execution.
    - Implemented `fetch_object_data_async` method for asynchronous data fetching.
    - Implemented `fetch_and_process_async` and `query_by_classification_async` methods for asynchronous processing and querying.
    - Added throttling to limit the rate of requests in asynchronous mode.
    - Refactored existing methods to support both synchronous and asynchronous modes.
    
    * Adding doc strings and better exception handling
    
    Also fixed an issue in MetQuery where the query limit
    was not being enforced correctly.
    
    * Updating the README and cly.py
    
    Changed the limit flag to default to 80 search hits.

--- src/coding_interview/cli.py
+++ src/coding_interview/cli.py
@@ -1,11 +1,6 @@
-"""'
-NOTES:
-https://metmuseum.github.io/#object
-https://metmuseum.github.io/#search
-https://collectionapi.metmuseum.org/public/collection/v1/objects
-"""
-
+import asyncio
 import json
+import time
 
 import click
 
@@ -14,13 +9,22 @@ from .met_query import MetQuery
 
 @click.command()
 def print_total_objects():
+    """
+    Prints the total number of objects in the MetQuery.
+    """
     met_query = MetQuery()
-    total_objects = met_query.fetch_objects_total()
-    print(f"Total objects: {total_objects}")
+    try:
+        total_objects = met_query.fetch_objects_total()
+        print(f"Total objects: {total_objects}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
 @click.group()
 def met_cli():
+    """
+    Command line interface for MetQuery.
+    """
     pass
 
 
@@ -34,17 +38,62 @@ met_cli.add_command(print_total_objects)
     help="Input IDs to search for. Can be a single integer (e.g., '5'), a range of integers (e.g., '1-20') a list of integers (e.g., '1, 4, 6, 8'), or 0 for all object IDs.",
 )
 @click.option(
-    "--limit", default=None, type=int, help="Limit the number of results returned."
+    "--limit", default=80, type=int, help="Limit the number of results returned."
 )
 @click.option("--search_string", default=None, help="Search string for classification.")
 @click.option("--ascending", default=True, type=bool, help="Order of date of creation.")
-def classifications(id_input, limit, search_string, ascending):
-    met_query = MetQuery()
-    results = met_query.query_by_classification(
-        id_input, limit, search_string, ascending
-    )
-    for result in results:
-        print(json.dumps(result))
+@click.option("--debug", default=False, is_flag=True, help="Print debug information.")
+@click.option("--use_async", default=False, is_flag=True, help="Use async execution.")
+def classifications(id_input, limit, search_string, ascending, debug, use_async):
+    """
+    Fetches and prints classifications based on the provided parameters.
+
+    Parameters:
+    id_input (str): The input IDs to search for.
+    limit (int): The limit on the number of results.
+    search_string (str): The search string for classification.
+    ascending (bool): The order of date of creation.
+    debug (bool): Whether to print debug information.
+    use_async (bool): Whether to use async execution.
+    """
+    start_time = time.time()
+    try:
+        if use_async:
+            asyncio.run(_classifications(id_input, limit, search_string, ascending))
+        else:
+            met_query = MetQuery(use_async=False)
+            results = met_query.query_by_classification(
+                id_input, limit, search_string, ascending
+            )
+            for result in results:
+                print(json.dumps(result))
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        end_time = time.time()
+        if debug:
+            print(f"Time taken: {end_time - start_time} seconds")
+
+
+async def _classifications(id_input, limit, search_string, ascending):
+    """
+    Fetches and prints classifications asynchronously based on the provided parameters.
+
+    Parameters:
+    id_input (str): The input IDs to search for.
+    limit (int): The limit on the number of results.
+    search_string (str): The search string for classification.
+    ascending (bool): The order of date of creation.
+    """
+    try:
+        met_query = MetQuery(use_async=True)
+        results = await met_query.query_by_classification(
+            id_input, limit, search_string, ascending
+        )
+        for result in results:
+            print(json.dumps(result))
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
 met_cli.add_command(classifications)
--- src/coding_interview/met_query.py
+++ src/coding_interview/met_query.py
@@ -1,14 +1,74 @@
+import asyncio
+
+import aiohttp
 import requests
-from ratelimit import limits, sleep_and_retry
+from aiohttp.client_exceptions import ClientConnectorError
+from asyncio_throttle import Throttler
+from tenacity import retry, stop_after_attempt, wait_fixed
+
+
+class InvalidInputError(Exception):
+    """Raised when the input to a function is not as expected."""
+
+    pass
 
 
 class MetQuery:
+    """
+    A class used to query the Met Museum's collection API.
+
+    ...
+
+    Attributes
+    ----------
+    collections_endpoint : str
+        the endpoint to query the Met Museum's collection API
+    use_async : bool
+        a flag indicating whether to use async or sync requests
+    throttler : Throttler
+        a throttler to limit the rate of requests
+
+    Methods
+    -------
+    handle_response(response)
+        Handles the response from the API.
+    fetch_object_data_async(object_id, semaphore)
+        Fetches the data for a specific object asynchronously.
+    fetch_object_data_sync(object_id)
+        Fetches the data for a specific object synchronously.
+    fetch_object_data(object_id, semaphore=None)
+        Fetches the data for a specific object.
+    fetch_objects_total()
+        Fetches the total number of objects in the collection.
+    parse_ids(id_input)
+        Parses the input IDs.
+    fetch_and_process_async(obj, query_results, search_string, semaphore)
+        Fetches and processes the data for a specific object asynchronously.
+    fetch_and_process_sync(obj, query_results, search_string)
+        Fetches and processes the data for a specific object synchronously.
+    fetch_and_process(obj, query_results, search_string, semaphore=None)
+        Fetches and processes the data for a specific object.
+    query_by_classification_async(id_input=None, limit=None, search_string=None, ascending=True)
+        Queries the collection by classification asynchronously.
+    query_by_classification_sync(id_input=None, limit=None, search_string=None, ascending=True)
+        Queries the collection by classification synchronously.
+    query_by_classification(id_input=None, limit=None, search_string=None, ascending=True)
+        Queries the collection by classification.
+    """
+
     collections_endpoint = (
         "https://collectionapi.metmuseum.org/public/collection/v1/objects"
     )
 
+    def __init__(self, use_async=False):
+        """Initializes the MetQuery class."""
+        self.use_async = use_async
+        if self.use_async:
+            self.throttler = Throttler(rate_limit=40)  # Limit to 40 requests per second
+
     @staticmethod
     def handle_response(response):
+        """Handles the response from the API."""
         if response.status_code == 200:
             return response.json()
         elif response.status_code == 502:
@@ -18,16 +78,41 @@ class MetQuery:
             print(f"Error: {response.status_code}")
             return None
 
-    @classmethod
-    @sleep_and_retry
-    @limits(calls=80, period=1)
-    def fetch_object_data(self, object_id):
+    @retry(stop=stop_after_attempt(3), wait=wait_fixed(1))
+    async def fetch_object_data_async(self, object_id, semaphore):
+        """Fetches the data for a specific object asynchronously."""
         url = f"{self.collections_endpoint}/{object_id}"
-        response = requests.get(url)
-        return response.json()
+        async with semaphore, self.throttler:
+            try:
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(url) as response:
+                        return await response.json()
+            except ClientConnectorError:
+                print(
+                    f"Failed to connect to host for object ID: {object_id}. Retrying..."
+                )
+                raise
+
+    def fetch_object_data_sync(self, object_id):
+        """Fetches the data for a specific object synchronously."""
+        url = f"{self.collections_endpoint}/{object_id}"
+        try:
+            response = requests.get(url)
+            return self.handle_response(response)
+        except requests.exceptions.RequestException as e:
+            print(f"Failed to connect to host for object ID: {object_id}. Retrying...")
+            raise
+
+    def fetch_object_data(self, object_id, semaphore=None):
+        """Fetches the data for a specific object."""
+        if self.use_async:
+            return self.fetch_object_data_async(object_id, semaphore)
+        else:
+            return self.fetch_object_data_sync(object_id)
 
     @classmethod
     def fetch_objects_total(self):
+        """Fetches the total number of objects in the collection."""
         url = f"{self.collections_endpoint}"
         response = requests.get(url)
         data = self.handle_response(response)
@@ -35,6 +120,7 @@ class MetQuery:
 
     @classmethod
     def parse_ids(self, id_input):
+        """Parses the input IDs."""
         if id_input == 0 or id_input is None:
             total_objects = self.fetch_objects_total()
             return iter(range(1, total_objects + 1))
@@ -55,12 +141,142 @@ class MetQuery:
         elif isinstance(id_input, list):
             return iter(id_input)
         else:
-            raise ValueError("Invalid input type. Expected int, str, or list.")
+            raise InvalidInputError("Invalid input type. Expected int, str, or list.")
 
-    @classmethod
-    def query_by_classification(
+    async def fetch_and_process_async(
+        self, obj, query_results, search_string, semaphore, limit=None
+    ):
+        """
+        Fetches and processes the data for a specific object asynchronously.
+
+        Parameters:
+        obj (int): The object ID.
+        query_results (asyncio.Queue): The queue to append the query results to.
+        search_string (str): The search string to match in the classification.
+        semaphore (asyncio.Semaphore): The semaphore to limit the number of concurrent requests.
+        limit (int, optional): The limit on the number of results. Defaults to None.
+        """
+        if limit is not None and query_results.qsize() >= limit:
+            return
+
+        data = await self.fetch_object_data(obj, semaphore)
+        if data is None:
+            print(f"Failed to fetch data for object ID: {obj}")
+            return
+
+        primary_image = data.get("primaryImage")
+        primary_image_small = data.get("primaryImageSmall")
+        additional_images = data.get("additionalImages")
+        classification = data.get("classification")
+
+        if (
+            (primary_image or primary_image_small or additional_images)
+            and classification
+            and (search_string in classification)
+        ):
+            if limit is None or query_results.qsize() < limit:
+                await query_results.put(data)
+
+        await asyncio.sleep(0.5)  # Add a delay of seconds between each request
+
+    def fetch_and_process_sync(self, obj, query_results, search_string, limit=None):
+        """
+        Fetches and processes the data for a specific object synchronously.
+
+        Parameters:
+        obj (int): The object ID.
+        query_results (list): The list to append the query results to.
+        search_string (str): The search string to match in the classification.
+        limit (int, optional): The limit on the number of results. Defaults to None.
+        """
+        if limit is not None and len(query_results) >= limit:
+            return
+        data = self.fetch_object_data(obj)
+        if data is None:
+            print(f"Failed to fetch data for object ID: {obj}")
+            return
+
+        primary_image = data.get("primaryImage")
+        primary_image_small = data.get("primaryImageSmall")
+        additional_images = data.get("additionalImages")
+        classification = data.get("classification")
+
+        if (
+            (primary_image or primary_image_small or additional_images)
+            and classification
+            and (search_string in classification)
+        ):
+            query_results.append(data)
+
+    def fetch_and_process(self, obj, query_results, search_string, semaphore=None):
+        """
+        Fetches and processes the data for a specific object.
+
+        Parameters:
+        obj (int): The object ID.
+        query_results (list): The list to append the query results to.
+        search_string (str): The search string to match in the classification.
+        semaphore (asyncio.Semaphore, optional): The semaphore to limit the number of concurrent requests. Defaults to None.
+        """
+        if self.use_async:
+            return self.fetch_and_process_async(
+                obj, query_results, search_string, semaphore
+            )
+        else:
+            return self.fetch_and_process_sync(obj, query_results, search_string)
+
+    import asyncio
+
+    async def query_by_classification_async(
         self, id_input=None, limit=None, search_string=None, ascending=True
     ):
+        """
+        Queries the collection by classification asynchronously.
+
+        Parameters:
+        id_input (int, optional): The input ID. Defaults to None.
+        limit (int, optional): The limit on the number of results. Defaults to None.
+        search_string (str, optional): The search string to match in the classification. Defaults to None.
+        ascending (bool, optional): Whether to sort the results in ascending order. Defaults to True.
+        """
+        if id_input is None:
+            id_input = self.fetch_objects_total()
+        ids = self.parse_ids(id_input)
+
+        # Create an asyncio Queue with maxsize equal to limit
+        query_results = asyncio.Queue(maxsize=limit)
+
+        semaphore = asyncio.Semaphore(40)  # Limit to 40 requests per second
+        await asyncio.gather(
+            *(
+                self.fetch_and_process_async(
+                    obj, query_results, search_string, semaphore, limit
+                )
+                for obj in ids
+            )
+        )
+
+        # Get the results from the queue
+        results = []
+        while not query_results.empty():
+            results.append(await query_results.get())
+
+        results.sort(key=lambda x: x.get("objectBeginDate"), reverse=not ascending)
+
+        return results
+
+    def query_by_classification_sync(
+        self, id_input=None, limit=None, search_string=None, ascending=True
+    ):
+        """
+        Queries the collection by classification synchronously.
+
+        Parameters:
+        id_input (int, optional): The input ID. Defaults to None.
+        limit (int, optional): The limit on the number of results. Defaults to None.
+        search_string (str, optional): The search string to match in the classification. Defaults to None.
+        ascending (bool, optional): Whether to sort the results in ascending order. Defaults to True.
+        """
         if id_input is None:
             id_input = self.fetch_objects_total()
         ids = self.parse_ids(id_input)
@@ -69,26 +285,31 @@ class MetQuery:
         for obj in ids:
             if limit is not None and len(query_results) >= limit:
                 break
-
-            data = self.fetch_object_data(obj)
-            if data is None:
-                print(f"Failed to fetch data for object ID: {obj}")
-                continue
-
-            primary_image = data.get("primaryImage")
-            primary_image_small = data.get("primaryImageSmall")
-            additional_images = data.get("additionalImages")
-            classification = data.get("classification")
-
-            if (
-                (primary_image or primary_image_small or additional_images)
-                and classification
-                and (search_string in classification)
-            ):
-                query_results.append(data)
+            self.fetch_and_process(obj, query_results, search_string)
 
         query_results.sort(
             key=lambda x: x.get("objectBeginDate"), reverse=not ascending
         )
 
         return query_results
+
+    def query_by_classification(
+        self, id_input=None, limit=None, search_string=None, ascending=True
+    ):
+        """
+        Queries the collection by classification.
+
+        Parameters:
+        id_input (int, optional): The input ID. Defaults to None.
+        limit (int, optional): The limit on the number of results. Defaults to None.
+        search_string (str, optional): The search string to match in the classification. Defaults to None.
+        ascending (bool, optional): Whether to sort the results in ascending order. Defaults to True.
+        """
+        if self.use_async:
+            return self.query_by_classification_async(
+                id_input, limit, search_string, ascending
+            )
+        else:
+            return self.query_by_classification_sync(
+                id_input, limit, search_string, ascending
+            )
